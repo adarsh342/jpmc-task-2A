5585e8c65066d7b004997c3413c1036ab8e2c457
adarsh342 <126852443+adarsh342@users.noreply.github.com>
2023-03-07 17:20:37 +0530

    Update DataStreamer.ts

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -6,32 +6,35 @@ export interface Order {
  * The datafeed server returns an array of ServerRespond with 2 stocks.
  * We do not have to manipulate the ServerRespond for the purpose of this task.
  */
- export interface Row {
-  price_abc: number,
-  price_def: number,
-  ratio: number,
+export interface ServerRespond {
+  stock: string,
+  top_bid: Order,
+  top_ask: Order,
   timestamp: Date,
-  upper_bound: number,
-  lower_bound: number,
-  trigger_alert: number | undefined,
 }
-export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]): Row {
-    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
-    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
-    const ratio = priceABC / priceDEF;
-    const uperBound = 1 + 0.05;
-    const lowerBound = 1 - 0.05
-    return {
-      price_abc: priceABC,
-      price_def: priceDEF,
-      ratio,
-      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?
-          serverResponds[0].timestamp : serverResponds[1].timestamp,
-      upper_bound: uperBound,
-      lower_bound: lowerBound,
-      trigger_alert: (ratio > uperBound || ratio < lowerBound) ? ratio : undefined,
-    };
+
+class DataStreamer {
+  // The url where datafeed server is listening
+  static API_URL: string = 'http://localhost:8080/query?id=1';
+
+  /**
+   * Send request to the datafeed server and executes callback function on success
+   * @param callback callback function that takes JSON object as its argument
+   */
+  static getData(callback: (data: ServerRespond[]) => void): void {
+    const request = new XMLHttpRequest();
+    request.open('GET', DataStreamer.API_URL, false);
+
+    request.onload = () => {
+      if (request.status === 200) {
+        callback(JSON.parse(request.responseText));
+      } else {
+        alert ('Request failed');
+      }
+    }
+
+    request.send();
   }
 }
-  
+
+export default DataStreamer;
